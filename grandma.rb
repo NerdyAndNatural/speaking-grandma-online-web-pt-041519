671338e1208cb57ef17f2a60a4843d581e7d49da
Sarah Williams <srwilliams8678@gmail.com>
2019-04-17 00:39:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,9 @@
 
 def speak_to_grandma (sonny)
 if sonny == sonny.uppercase
-  puts "HUH? SPEAK UP SONNY!"
-  else
+  puts "NO! NOT SINCE 1938!"
+elseif sonny == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+else
     puts "HUH?! SPEAK UP SONNY!"
 end
