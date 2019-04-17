5511e160b6684462045628196b29b8a9ed174def
Sarah Williams <srwilliams8678@gmail.com>
2019-04-17 00:36:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma ()
+def speak_to_grandma (phrase)
   if sonny == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
  elseif sonny == "I LOVE YOU GRANDMA!"
