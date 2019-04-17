078f29dd0bda24a76975d2a66f5f6a41eb37b5ba
Sarah Williams <srwilliams8678@gmail.com>
2019-04-17 00:31:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
   if SONNY == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
  elseif SONNY == "I LOVE YOU GRANDMA!"
