745c2a10fd90865e0be4f7f0af32c2504bafedc3
Sarah Williams <srwilliams8678@gmail.com>
2019-04-17 00:40:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 
 def speak_to_grandma (sonny)
-  if sonny == sonny.uppercase
+  if sonny == sonny.upcase
     puts "NO! NOT SINCE 1938!"
   elseif sonny == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
